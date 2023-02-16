be522ccab247a3c6f46db0aa923f24f47e82e8b4
Evgenii Tarasov <mailtarasov@yandex.ru>
2023-02-16 22:03:21 +0700

    refactor: Исправить замечания ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,6 +9,7 @@ const userLogin = document.querySelector(".profile__login");
 const userActivity = document.querySelector(".profile__activity");
 
 const profileForm = document.querySelector(".popup__form_profile");
+const cardForm = document.querySelector(".popup__form_add-cards");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_data"); 
 const templateCard = document.querySelector('.card').content.querySelector('.element');
@@ -16,25 +17,25 @@ const templateCard = document.querySelector('.card').content.querySelector('.ele
 const cardsContainer = document.querySelector('.elements__list'); 
 const inputTitle = document.querySelector('.popup__input_type_title');
 const inputLink = document.querySelector('.popup__input_type_link');
-const cardFormSubmitButton = document.querySelector('.popup__save_create');
 
 const cardDataLink = popupFormImg.querySelector('.popup__img')
 const cardDataName =  popupFormImg.querySelector('.popup__img')
 const CardDataContent = popupFormImg.querySelector('.popup__description')
-const popupContener = document.querySelectorAll('.popup')
+const popupList = document.querySelectorAll('.popup')
 
-popupContener.forEach((popup) =>{
-  const popupBtnClose = popup.querySelector('.popup__close')
+
+
+popupList.forEach((popup) =>{
   popup.addEventListener('click', function(evt) {
-    if (evt.target === popupBtnClose || evt.target === evt.currentTarget) {
+    if ( evt.target.classList.contains('popup__close') || evt.target === evt.currentTarget) {
       closePopup(popup);
     }  
   });
 });
 
 function closeEscapePopup(evt) {
-  const popupOpen = document.querySelector('.popup_opened')
   if (evt.key === 'Escape') {
+    const popupOpen = document.querySelector('.popup_opened')
     closePopup(popupOpen); 
   }  
 }
@@ -52,7 +53,7 @@ function openProfilePopup(popup) {
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
-  document.removeEventListener('keydown', closePopupEscape); 
+  document.removeEventListener('keydown', closeEscapePopup); 
 }
 
 function handleProfileFormSubmit (evt) {
@@ -71,22 +72,22 @@ function renderInitialCards() {
   cardsContainer.append(...cards); 
 }
 
-cardFormSubmitButton.addEventListener('click', (evt) => {
+cardForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const name = inputTitle.value;
   const link = inputLink.value;
   const cardElement = createCard({name: name, link: link});
   cardsContainer.prepend(cardElement); 
   closePopup(popupFormAddCards);
-  inputTitle.value = "";
-  inputLink.value = "";
+  cardForm.reset();
 });
 
 function createCard(cardData) {
   const cardElement = templateCard.cloneNode(true);
+  const elementImg = cardElement.querySelector('.element__img');
     cardElement.querySelector('.element__title').textContent = cardData.name;
-    cardElement.querySelector('.element__img').src = cardData.link;
-    cardElement.querySelector('.element__img').alt = cardData.name;
+    elementImg.src = cardData.link;
+    elementImg.alt = cardData.name;
     cardElement.querySelector('.element__delete').addEventListener('click',() => {
       cardElement.remove();
     });
@@ -96,7 +97,7 @@ function createCard(cardData) {
       buttonLike.classList.toggle("element__button_active");
     });   
     
-    cardElement.querySelector('.element__img').addEventListener('click',() => {
+    elementImg.addEventListener('click',() => {
       openPopup(popupFormImg)           
       cardDataLink.src = cardData.link;
       cardDataName.alt = cardData.name;
@@ -110,4 +111,3 @@ enableValidation(formsConfig);
 popupBtnOpenProfile.addEventListener("click", () => openProfilePopup(popupFormProfile));
 popupBtnOpenAddCard.addEventListener("click", () => openPopup(popupFormAddCards));
 profileForm.addEventListener('submit', handleProfileFormSubmit);
-
