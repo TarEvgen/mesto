f84210336a384a300b60f7d7bb84e09911257a51
Evgenii Tarasov <mailtarasov@yandex.ru>
2023-04-10 21:09:34 +0700

    исправить замечания ревью

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ export class Card {
   constructor(
     cardData,
     templateSelector,
-    { openImg, openDeleteCard, addLike, deleteLike, deleteButton },
+    { openImg, openDeleteCard, addLike, deleteLike},
     userId
   ) {
     this._name = cardData.name;
@@ -15,8 +15,7 @@ export class Card {
     this._openImg = openImg;
     this._deleteCard = openDeleteCard;
     this._element = this._getTemplate();
-    this._deleteButton = deleteButton;
-
+    
     this._imageElement = this._element.querySelector(".element__img");
     this._elementButtonLike = this._element.querySelector(
       ".element__like-button"
